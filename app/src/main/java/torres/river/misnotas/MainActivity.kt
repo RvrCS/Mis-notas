350e822e3a6e79abcea21aa232056ba4bd3ffce3
River <75063620+RvrCS@users.noreply.github.com>
2023-03-06 18:28:27 -0700

    Initial commit

---
+++ app/src/main/java/torres/river/misnotas/MainActivity.kt
@@ -0,0 +1,11 @@
+package torres.river.misnotas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
